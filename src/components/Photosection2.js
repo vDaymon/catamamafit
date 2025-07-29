def3a69c23ae6a9924bed40fd0c9c7accecd7c3e
Victor Hugo Chaverz Martinez <81640621+vDaymon@users.noreply.github.com>
2025-07-28 22:22:57 -0500

    cambios 5

--- src/components/Photosection2.js
+++ src/components/Photosection2.js
@@ -9,6 +9,14 @@ const fadeIn = {
     transition: { delay: i * 0.2, duration: 0.7, type: "spring" }
   })
 };
+const fadeIn2 = {
+  hidden: { opacity: 0, y: 40 },
+  visible: (i = 1) => ({
+    opacity: 1,
+    y: 0,
+    transition: { delay: i * 0.6, duration: 0.7, type: "spring" }
+  })
+};
 
 const Photosection = ({
   image1 = "https://i.imgur.com/HcKtUa0.png",
@@ -19,12 +27,12 @@ const Photosection = ({
 }) => {
   return (
     <section className="py-16 bg-primary-dark">
-      <h2 className="text-3xl  text-center mb-8 text-white uppercase font-bold">¿Qué incluye el reto?</h2>
-  <div className="container mx-auto px-4 flex flex-col md:flex-row items-stretch gap-10 md:gap-16">
+      <h2 className="text-3xl  text-center  text-white uppercase font-bold">¿Qué incluye el reto?</h2>
+  <div className="container mx-auto px-4 flex flex-col md:flex-row items-stretch ">
     
     {/* Imagen 1 */}
     <motion.div
-      className="w-full md:w-1/2 h-full overflow-hidden  self-stretch"
+      className="w-full md:w-1/2 h-full overflow-hidden  self-stretch px-4"
       initial="hidden"
       whileInView="visible"
       viewport={{ once: true, amount: 0.3 }}
@@ -42,11 +50,11 @@ const Photosection = ({
 
     {/* Imagen 2 */}
     <motion.div
-      className="w-full md:w-1/2 h-full overflow-hidden  self-stretch"
+      className="w-full md:w-1/2 h-full overflow-hidden  self-stretch "
       initial="hidden"
       whileInView="visible"
       viewport={{ once: true, amount: 0.3 }}
-      variants={fadeIn}
+      variants={fadeIn2}
       custom={1}
     >
       <div className="w-full h-full">
